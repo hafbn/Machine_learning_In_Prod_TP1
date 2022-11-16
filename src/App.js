f6b86382e3128dca214674c9eb3e947c804fa661
HA Bao Ngoc <gbn131100@gmail.com>
2022-11-16 16:58:14 +0100

    Update App.js
    Add a form

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ function App() {
   }
   return (
     <form onSubmit={handleSubmit}>
+      <label> Name: <input type="text" name="name" /> </label>
       <button type="submit">Submit</button>
     </form>
   );
